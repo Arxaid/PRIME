9278670fa51d53ad26eff7b6a8a172207462929f
Arxaid <VallravenWasTaken@gmail.com>
2024-03-21 17:16:09 +0300

    Switching to 2nd PC (No sense, yeah)

--- main.py
+++ main.py
@@ -4,15 +4,56 @@
 # Licensed under the Apache License, Version 2.0.
 
 from src.rtcmDecoder import PRIME
-from pyrtcm import RTCMReader
+import numpy as np
+import math
 
 if __name__ == '__main__':
     primeDecoder = PRIME()
     msglen, msgcontent = primeDecoder.getmsg('1019.rtcm')
-    msgdecoded = primeDecoder.decodemsg(msgcontent)
+    msgvalues = primeDecoder.decodemsg(msgcontent)
+    primeDecoder.getSatelliteCoords(msgvalues)
 
-    # print('============== Проверка корректности ==============')
-    # stream = open('1019.rtcm', 'rb')
-    # rtr = RTCMReader(stream)
-    # for (raw_data, parsed_data) in rtr: print(parsed_data)
-    # print('===================================================')
+    print('====================== CALC ==================')
+    GM = 3.986005e14
+    omega_eps = 7.292115e-5
+    e = 0.008834721171297133
+
+    A = math.pow(5153.677635192871, 2)
+    print(f'Semi-major axis: {A}')
+    n0 = math.sqrt(GM/math.pow(A, 3))
+    print(f'Computed mean motion: {n0}')
+    n = n0 + 4.367324773749724e-09
+    print(f'Corrected mean motion: {n}')
+    Mk = 0.4945540624703678 + n*(-10)
+    print(f'Mean anomaly: {Mk}')
+    Ek0 = Mk
+    Ek1 = Mk + e*math.sin(Ek0)
+    Ek2 = Mk + e*math.sin(Ek1)
+    Ek3 = Mk + e*math.sin(Ek2)
+    Ek4 = Mk + e*math.sin(Ek3)
+    Ek = Ek4
+    print(f'Eccentric anomaly: {Ek}')
+    vkcos = (math.cos(Ek) - e)/(1 - e*math.cos(Ek))
+    vksin = (math.sqrt(1 - math.pow(e, 2))*math.sin(Ek))/(1 - e*math.cos(Ek))
+    print(f'True anomaly: cos {vkcos}, sin {vksin}')
+    Fk = math.acos(vkcos) + 0.5666441652896561
+    print(f'Argument of Latitude: {Fk}')
+    delta_uk = 3.46451997756958e-06*math.cos(2*Fk) + 3.2223761081695557e-07*math.sin(2*Fk)
+    uk = Fk + delta_uk
+    print(f'Argument of latitude correction: {delta_uk}, corrected argument of latitude: {uk}')
+    delta_rk = 381.65625*math.cos(2*Fk) + 66.59375*math.sin(2*Fk)
+    rk = A*(1-e*math.cos(Ek)) + delta_rk
+    print(f'Radius correction: {delta_rk}, corrected radius: {rk}')
+    delta_ik = 3.5390257835388184e-08*math.cos(2*Fk) + 1.3224780559539795e-07*math.sin(2*Fk)
+    ik = 0.9768164824302019 + 1.4786330195717906e-10 + delta_ik
+    print(f'Inclination correction: {delta_ik}, corrected inclination: {ik}')
+    Xk1 = rk*math.cos(uk)
+    Yk1 = rk*math.sin(uk)
+    print(f'Position in the orbital plane: {Xk1}, {Yk1}')
+    Omegak = -2.113868738843562 + (8.409993167115206e-09 - omega_eps)*(-10) - omega_eps*122384
+    print(f'Corrected longitude of ascending node: {Omegak}')
+
+    Xk = Xk1*math.cos(Omegak) - Yk1*math.sin(Omegak)*math.cos(ik)
+    Yk = Xk1*math.sin(Omegak) + Yk1*math.cos(Omegak)*math.cos(ik)
+    Zk = Yk1*math.sin(ik)
+    print(f'Earth-fixed geocentric satellite coordinate:\nX:{Xk}, Y:{Yk}, Z:{Zk}')
--- src/rtcmDecoder.py
+++ src/rtcmDecoder.py
@@ -5,6 +5,7 @@
 
 import bitstruct
 import crcmod
+import math
 
 class PRIME:
     def __init__(self):
@@ -16,6 +17,7 @@ class PRIME:
         self.msgcontent = ''
         self.msgoutput = ''
         self.msgdecoded = None
+        self.msgvalues = []
     
     def getmsg(self, path):
         with open(path, 'rb') as file:
@@ -26,22 +28,98 @@ class PRIME:
             self.crcValue = self.crcFunc(data[0:6 + self.msglen])
             if self.crcValue == 0:
                 self.msgcontent = data[3:3 + self.msglen]
-
         return self.msglen, self.msgcontent
 
-    def decodemsg(self, msgcontent):
+    def decodemsg(self, msgcontent: bytes):
         self.msgtype = bitstruct.unpack('u12', msgcontent)[0]
         self.msgdecodestr, self.msgoutput, self.coeff = _getcontent(self.msgtype)
         self.msgdecoded = bitstruct.unpack(self.msgdecodestr, msgcontent)
-        
         counter = 0
         for value in self.msgdecoded:
-            print(f'{self.msgoutput[counter]}{value * self.coeff[counter]}')
+            tuple_element = (self.msgoutput[counter], value * self.coeff[counter])
+            self.msgvalues.append(tuple_element)
             counter += 1
-
-        return self.msgdecoded
+        return self.msgvalues
     
+    def getSatelliteCoords(self, values: tuple):
+        GM = 3.986005e14
+        OMEGA_EPS = 7.292115e-5
+
+        A = math.pow(values[18][1], 2)
+        n0 = math.sqrt(GM/math.pow(A, 3))
+        n = n0 + values[13][1]
+        Mk = values[12][1] + n*(-10)
+        Ek0 = Mk
+        Ek1 = Mk + values[16][1]*math.sin(Ek0)
+        Ek2 = Mk + values[16][1]*math.sin(Ek1)
+        Ek3 = Mk + values[16][1]*math.sin(Ek2)
+        Ek4 = Mk + values[16][1]*math.sin(Ek3)
+        Ek = Ek4
+        vkcos = (math.cos(Ek) - values[16][1])/(1 - values[16][1]*math.cos(Ek))
+        vksin = (math.sqrt(1 - math.pow(values[16][1], 2))*math.sin(Ek))/(1 - values[16][1]*math.cos(Ek))
+        Fk = math.acos(vkcos) + values[25][1]
+        delta_uk = values[15][1]*math.cos(2*Fk) + values[17][1]*math.sin(2*Fk)
+        uk = Fk + delta_uk
+        delta_rk = values[12][1]*math.cos(2*Fk) + values[24][1]*math.sin(2*Fk)
+        rk = A*(1 - values[16][1]*math.cos(Ek)) + delta_rk
+        delta_ik = values[20][1]*math.cos(2*Fk) + values[22][1]*math.sin(2*Fk)
+        ik = values[23][1] + values[5][1] + delta_ik
+        Xk1 = rk*math.cos(uk)
+        Yk1 = rk*math.sin(uk)
+        Omegak = values[21][1] + (values[26][1] - OMEGA_EPS)*(-10) - OMEGA_EPS*values[19][1]
+
+        Xk = Xk1*math.cos(Omegak) - Yk1*math.sin(Omegak)*math.cos(ik)
+        Yk = Xk1*math.sin(Omegak) + Yk1*math.cos(Omegak)*math.cos(ik)
+        Zk = Yk1*math.sin(ik)
+        print(f'Earth-fixed geocentric satellite coordinate:\nX:{Xk}, Y:{Yk}, Z:{Zk}') 
+
+
+    # GM = 3.986005e14
+    # omega_eps = 7.292115e-5
+    # e = 0.008834721171297133
+
+    # A = math.pow(5153.677635192871, 2)
+    # print(f'Semi-major axis: {A}')
+    # n0 = math.sqrt(GM/math.pow(A, 3))
+    # print(f'Computed mean motion: {n0}')
+    # n = n0 + 4.367324773749724e-09
+    # print(f'Corrected mean motion: {n}')
+    # Mk = 0.4945540624703678 + n*(-10)
+    # print(f'Mean anomaly: {Mk}')
+    # Ek0 = Mk
+    # Ek1 = Mk + e*math.sin(Ek0)
+    # Ek2 = Mk + e*math.sin(Ek1)
+    # Ek3 = Mk + e*math.sin(Ek2)
+    # Ek4 = Mk + e*math.sin(Ek3)
+    # Ek = Ek4
+    # print(f'Eccentric anomaly: {Ek}')
+    # vkcos = (math.cos(Ek) - e)/(1 - e*math.cos(Ek))
+    # vksin = (math.sqrt(1 - math.pow(e, 2))*math.sin(Ek))/(1 - e*math.cos(Ek))
+    # print(f'True anomaly: cos {vkcos}, sin {vksin}')
+    # Fk = math.acos(vkcos) + 0.5666441652896561
+    # print(f'Argument of Latitude: {Fk}')
+    # delta_uk = 3.46451997756958e-06*math.cos(2*Fk) + 3.2223761081695557e-07*math.sin(2*Fk)
+    # uk = Fk + delta_uk
+    # print(f'Argument of latitude correction: {delta_uk}, corrected argument of latitude: {uk}')
+    # delta_rk = 381.65625*math.cos(2*Fk) + 66.59375*math.sin(2*Fk)
+    # rk = A*(1-e*math.cos(Ek)) + delta_rk
+    # print(f'Radius correction: {delta_rk}, corrected radius: {rk}')
+    # delta_ik = 3.5390257835388184e-08*math.cos(2*Fk) + 1.3224780559539795e-07*math.sin(2*Fk)
+    # ik = 0.9768164824302019 + 1.4786330195717906e-10 + delta_ik
+    # print(f'Inclination correction: {delta_ik}, corrected inclination: {ik}')
+    # Xk1 = rk*math.cos(uk)
+    # Yk1 = rk*math.sin(uk)
+    # print(f'Position in the orbital plane: {Xk1}, {Yk1}')
+    # Omegak = -2.113868738843562 + (8.409993167115206e-09 - omega_eps)*(-10) - omega_eps*122384
+    # print(f'Corrected longitude of ascending node: {Omegak}')
+
+    # Xk = Xk1*math.cos(Omegak) - Yk1*math.sin(Omegak)*math.cos(ik)
+    # Yk = Xk1*math.sin(Omegak) + Yk1*math.cos(Omegak)*math.cos(ik)
+    # Zk = Yk1*math.sin(ik)
+    # print(f'Earth-fixed geocentric satellite coordinate:\nX:{Xk}, Y:{Yk}, Z:{Zk}')   
+
 def _getcontent(msgtype):
+    pi = 3.1415926535898
     P2_P4 = 16
     P2_4 = 0.0625
     P2_5 = 0.03125
@@ -78,7 +156,8 @@ def _getcontent(msgtype):
                      'IDOT: ', 'IODE: ', 'toc: ', 'af2: ', 'af1: ', 'af0: ', 'IODC: ', 'Crs: ', '∆n: ', 'M0: ',
                      'Cuc: ', 'e: ', 'Cus: ', 'A½: ', 'toe: ', 'Cic: ', 'Ω0: ', 'Cis: ', 'i0: ', 'Crc: ', 'ω: ',
                      'Ω.: ', 'tGD: ', 'SV HEALTH: ', 'L2 P data flag: ', 'Fit Interval: ']
-        coeff =     [1, 1, 1, 1, 1, P2_43, 1, P2_P4, P2_55, P2_43, P2_31, 1, P2_5, P2_43, P2_31, P2_29, P2_33,
-                     P2_29, P2_19, P2_P4, P2_29, P2_31, P2_29, P2_31, P2_5, P2_31, P2_43, P2_31, 1, 1, 1]
+        coeff =     [1, 1, 1, 1, 1, P2_43*pi, 1, P2_P4, P2_55, P2_43, P2_31, 1, P2_5, P2_43*pi, P2_31*pi, P2_29, P2_33,
+                     P2_29, P2_19, P2_P4, P2_29, P2_31*pi, P2_29, P2_31*pi, P2_5, P2_31*pi, P2_43*pi, P2_31, 1, 1, 1]
+        
     
     return msgdecodestr, msgoutput, coeff
